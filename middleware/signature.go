cff068950edf69e89ef610ae54e5c05537de99e6
Adam Cooper <adam@networkpie.co.uk>
2018-05-28 20:59:24 +0100

    Simplify the hash construction.

--- middleware/signature.go
+++ middleware/signature.go
@@ -1,18 +1,20 @@
 package middleware
 
 import (
-	"bytes"
 	"crypto/sha512"
 	"encoding/base64"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/cooperaj/starling-coinjar/util"
 )
 
+// SignatureValidationMiddleware Provides the webhook authentication that Starling uses
 type SignatureValidationMiddleware struct {
 	Secret string
 }
 
+// Middleware The middleware handler
 func (s *SignatureValidationMiddleware) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		hash := r.Header.Get("X-Hook-Signature")
@@ -27,12 +29,9 @@ func (s *SignatureValidationMiddleware) Middleware(next http.Handler) http.Handl
 
 		// encode our own version of the signature so we can check it against the
 		// provided one.
-		var b bytes.Buffer
-		b.WriteString(s.Secret)
-		b.ReadFrom(r.Body)
-
+		body, _ := ioutil.ReadAll(r.Body)
 		shaHash := sha512.New()
-		shaHash.Write(b.Bytes())
+		shaHash.Write([]byte(s.Secret + string(body)))
 
 		encodedString := base64.StdEncoding.EncodeToString(shaHash.Sum(nil))
 
